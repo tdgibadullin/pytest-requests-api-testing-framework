cb8cb71aea418499553f5c0f61306659a45a4bb8
tdgibadullin <tdgibadullin@gmail.com>
2025-05-25 01:16:30 +0300

    Initial commit for yandex_api_stand_tests

---
+++ configuration.py
@@ -0,0 +1,3 @@
+URL_SERVICE = "https://1a074d28-7417-49d1-a5a9-867868e8ec47.serverhub.praktikum-services.ru"
+USERS_TABLE_PATH = "/api/db/resources/user_model.csv"
+CREATE_USER_PATH = "/api/v1/users/"
